0246d9cff2f3d5b8af1e3e6f6749cec23688f788
hayden <1058486292@qq.com>
2023-04-27 15:50:46 +0800

    feat: 统一返回接口：1、添加全局过滤 2、添加全局拦截器

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@
  * @Author       : wuhaidong
  * @Date         : 2022-12-15 17:14:31
  * @LastEditors  : wuhaidong
- * @LastEditTime : 2023-04-27 10:18:11
+ * @LastEditTime : 2023-04-27 15:31:22
  */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
+import { HttpExceptionFilter } from './core/filter/httpException';
+import { TransformInterceptor } from './core/interceptor/transform';
 
 function MiddleWareAll(req: any, res: any, next: any) {
   console.log('全局中间件');
@@ -23,6 +25,10 @@ async function bootstrap() {
   app.use(cors());
   // 全局中间件
   app.use(MiddleWareAll);
+  // 全局接口返回过滤器
+  app.useGlobalFilters(new HttpExceptionFilter());
+  // 全局注册拦截器
+  app.useGlobalInterceptors(new TransformInterceptor());
   // 启动端口
   await app.listen(4000);
   // 热更新
