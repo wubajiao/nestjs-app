599076bfe0070030e856d2b54bba542c5a6cf5d4
hayden <1058486292@qq.com>
2023-04-27 10:06:10 +0800

    feat: 全局中间件：在main文件中加入MiddleWareAll

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,30 @@
  * @Author       : wuhaidong
  * @Date         : 2022-12-15 17:14:31
  * @LastEditors  : wuhaidong
- * @LastEditTime : 2023-04-25 16:50:38
+ * @LastEditTime : 2023-04-27 10:04:04
  */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+function MiddleWareAll(req: any, res: any, next: any) {
+  console.log('全局中间件');
+  // next() 如果不加的话就不会执行后面的了
+  next();
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 设置全局前缀
   app.setGlobalPrefix('/api');
+  // 全局中间件
+  app.use(MiddleWareAll);
+  // 启动端口
+  await app.listen(4000);
   // 热更新
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
-  // 启动端口
-  await app.listen(4000);
 }
 
 bootstrap();
